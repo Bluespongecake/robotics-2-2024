af4a84cfb035ca67cd9dbe712904d3c7fe53ddf3
de3robotics <david>
2024-01-22 15:21:21 +0000

    initial commit - completed tasks up to and including E

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/de3robotics/Desktop/DE3Robotics/src
+# Build directory: /home/de3robotics/Desktop/DE3Robotics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("coursework_1")
